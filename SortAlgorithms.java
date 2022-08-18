ceb5226a9767c044e010531bf4fcac12249198da
huynhnhu <nhunguyen339@gmail.com>
2022-08-19 01:08:29 +0700

    feat(sort algorithm): selection sort

--- SortAlgorithms.java
+++ SortAlgorithms.java
@@ -1,8 +1,10 @@
 public class SortAlgorithms {
     public static void main(String[] args) {
-        bubbleSort();
+//        bubbleSort();
+        selectionSort();
     }
 
+    // swap largest to sort partion
     public static void bubbleSort() {
         int[] intArray = {9,4,6,2,7,8,9,1};
 
@@ -13,6 +15,26 @@ public class SortAlgorithms {
                 }
             }
         }
+        printArray(intArray);
+    }
+
+
+    // select largest to swap to sort partion
+    public static void selectionSort() {
+        int[] intArray = {9,45,2,3,5,-1,-5};
+
+        for (int lastSortIndex = intArray.length - 1; lastSortIndex > 0; lastSortIndex -- ) {
+            int largest = 0;
+
+            for (int i = 0; i <= lastSortIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+
+            swap(lastSortIndex, largest, intArray);
+        }
+
         printArray(intArray);
     }
 
