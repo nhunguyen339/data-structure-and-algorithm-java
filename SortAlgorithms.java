2168f53d0f10230a3d1e40cccf918c20e55076dd
huynhnhu <nhunguyen339@gmail.com>
2022-09-07 23:54:10 +0700

    feat(sort algorithm): merge sort

--- SortAlgorithms.java
+++ SortAlgorithms.java
@@ -3,7 +3,8 @@ public class SortAlgorithms {
 //        bubbleSort();
 //        selectionSort();
 //        insertionSort();
-            shellSort();
+//            shellSort();
+        mergeSortPrint();
     }
 
     // swap largest to sort partion
@@ -95,5 +96,70 @@ public class SortAlgorithms {
 
     }
 
+    // Merge two subarrays L and M into arr
+    public static void merge(int arr[], int p, int q, int r) {
 
-}
+        // Create L ← A[p..q] and M ← A[q+1..r]
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int L[] = new int[n1];
+        int M[] = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[p + i];
+        for (int j = 0; j < n2; j++)
+            M[j] = arr[q + 1 + j];
+
+        // Maintain current index of sub-arrays and main array
+        int i, j, k;
+        i = 0;
+        j = 0;
+        k = p;
+
+        // Until we reach either end of either L or M, pick larger among
+        // elements L and M and place them in the correct position at A[p..r]
+        while (i < n1 && j < n2) {
+            if (L[i] <= M[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = M[j];
+                j++;
+            }
+            k++;
+        }
+
+        // When we run out of elements in either L or M,
+        // pick up the remaining elements and put in A[p..r]
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = M[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] arr, int p, int r) {
+        if (p < r) {
+            int m = (p + r) / 2;
+            mergeSort(arr, p , m);
+            mergeSort(arr, m + 1, r);
+            System.out.println("m: " + m);
+
+            merge(arr, p, m, r);
+        }
+    }
+
+    public static void mergeSortPrint() {
+        int[] intArray = {9,8, 5,3,7};
+        mergeSort(intArray, 0, intArray.length -1 );
+        printArray(intArray);
+    }
+
+    }
